b72f008702a0a86fb68d2e21b4d6d9443640daf1
allangabr <allanmediogames@gmail.com>
2025-04-24 18:49:52 -0300

    feat: implemented controller.ChatController

---
+++ src/main/java/com/furia/chatbot/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.furia.chatbot.controller;
+
+import com.furia.chatbot.model.Message;
+import com.furia.chatbot.service.ChatService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatbotService;
+
+    public ChatController(ChatService chatbotService) {
+        this.chatbotService = chatbotService;
+    }
+
+    @PostMapping
+    public Message sendMessage(@RequestBody Message userMessage) {
+        return chatbotService.getBotResponse(userMessage);
+    }
+}
